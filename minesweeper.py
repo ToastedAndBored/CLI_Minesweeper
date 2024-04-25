50cbd49f70e70406b46fa5f6311f6499db01e4fb
Toast <bored@toasted.me>
2024-04-25 23:22:15 +0400

    Working Game

--- minesweeper.py
+++ minesweeper.py
@@ -32,6 +32,16 @@ def remove_chars_whitelist(text):
 	return ret
 
 templates = ""
+
+
+def newBoard(y,x):
+	board = []
+	for yy in range(int(y)):
+		line = ['◻' for i in range(int(x))]
+		board.append(line)
+	return board
+
+"""
 def newBoard(y,x,template=DEFAULT):
 	if x ==0 and y == 0:
 		cleaned_template = remove_chars_whitelist(template)
@@ -51,8 +61,9 @@ def newBoard(y,x,template=DEFAULT):
 			line = ['◻' for i in range(int(x))]
 			board.append(line)
 		return board
+"""
+
 # ◼ ◻
-	pass
 
 def count(c, v):
 	ret = 0
@@ -63,127 +74,44 @@ def count(c, v):
 
 def printBoard(board):
 	result = ""
+	rown = 0
+	r = ""
 	for y, row in enumerate(board):
+		if rown in range(0,10):
+			result+=str(rown)+"  "
+			rown+=1
+		else:
+			result+=str(rown)+" "
+			rown+=1
 		for x, c in enumerate(row):
+			if c=="ы":
+				result+="\033[42m◻\033[0m"
+				continue
+			if c=="а":
+				result+="\033[42m◻\033[0m"
+				continue
 			if c==".":
 				result += "◻"
+				continue
+			n = get_neighbours(board, y, x)
+			co = count(n, ".")
+			if co > 0 and "◼" in n:
+				result += str(co)
 			else:
-				n = get_neighbours(board, y, x)
-				co = count(n, ".")
-				if co > 0 and "◼" in n:
-					result += str(co)
-				else:
-					result += c
+				result += c
 		result += '\n'
+	result += "   "
+	for i in range(len(board[0])):
+		result+=str(i%10)
+	result += "\n   "
+	for i in range(len(board[0])):
+		d = i//10
+		if d == 0:
+			result += " "
+			continue
+		result+=str(d)
 	return result
 
-def nearBomb(board,y,x) -> bool:
-	for yy in range(-1,1):
-		for xx in range(-1,1):
-			yy = yy + y
-			xx = xx + x
-		if board[yy][xx] == ".":
-			try:
-				return True
-			except:
-				pass
-		else:
-			try:
-				return False
-			except:
-				pass
-
-def neigboursIf(board, y, x):
-	ret = []
-	try:
-		if board[y-1][x-1] in '.◼◻':
-			try:
-				ret.append(board[y-1][x-1])
-			except:
-				pass
-		if board[y-1][x] in '.◼◻':
-			try:
-				ret.append(board[y-1][x])
-			except:
-				pass
-		if board[y-1][x+1] in '.◼◻':
-			try:
-				ret.append(board[y-1][x+1])
-			except:
-				pass
-		if board[y][x-1] in '.◼◻':
-			try:
-				ret.append(board[y][x-1])
-			except:
-				pass
-		if board[y][x+1] in '.◼◻':
-			try:
-				ret.append(board[y][x+1])
-			except:
-				pass
-		if board[y+1][x] in '.◼◻':
-			try:
-				ret.append(board[y+1][x])
-			except:
-				pass
-		if board[y+1][x-1] in '.◼◻':
-			try:
-				ret.append(board[y+1][x-1])
-			except:
-				pass
-		if board[y+1][x] in '.◼◻':
-			try:
-				ret.append(board[y+1][x])
-			except:
-				pass
-	except:
-		pass
-	return ret
-
-def neigbours(board, y, x):
-	ret = []
-	for yy in range(-1,1):
-		for xx in range(-1,1):
-			if yy == 0 and xx == 0:
-				continue
-			yy = yy + y
-			xx = xx + x
-			if (yy < 0 or xx < 0) or yy >= len(board) or xx >= len(board[0]):
-				continue
-			try:
-				ret.append(board[yy][xx])
-			except:
-				pass
-	return ret
-
-"""
-def firstTurn(board,step,bombs):
-	board[step[0]][step[1]]="◼"
-	palnted = 0
-	while palnted < bombs:
-		y, x = randint(0, len(board)-1), randint(0, len(board[0])-1)
-		if board[y][x] in ".◼":
-			continue
-		board[y][x] = "."
-		palnted+=1
-	print()
-	#board[step[0]][step[1]]="◻"
-	opened = True
-	while opened:
-		print(printBoard(board)+"\n")
-		sleep(0.1)
-		opened = False
-		for y in range(len(board)):
-			if opened:
-				break
-			for x in range(len(board[0])):
-				n = get_neighbours(board,y,x)
-				print(n)
-				if (board[y][x] == "◻") and ("." not in n):# and ("◼" in n):
-					opened = True
-					board[y][x] = "◼"
-					break
-"""
 def firstTurn(board,step,bombs):
 	board[step[0]][step[1]]="◼"
 	palnted = 0
@@ -211,7 +139,34 @@ def firstTurn(board,step,bombs):
 				#print(y, x)
 
 def turn(board,step,mod):
-	pass
+	print(step, mod, board[step[0]][step[1]])
+	if mod == 0: #digging mode
+		if board[step[0]][step[1]] not in".":
+			board[step[0]][step[1]]= "◼"
+			opened = True
+			while opened:
+				opened = False
+				for y in range(len(board)):
+					for x in range(len(board[0])):
+						n = get_neighbours(board,y,x)
+						if board[y][x] != "◻":
+							continue
+						if "◼" not in n:
+							continue
+						if "." in n:
+							continue
+						board[y][x] = "◼"
+						opened = True
+	if mod == 1: #marking mode
+		if board[step[0]][step[1]]==".":
+			board[step[0]][step[1]]= "ы"
+		else:
+			board[step[0]][step[1]]= "а"
+	if mod == 2: #unmarking mode
+		if board[step[0]][step[1]]=="ы":
+			board[step[0]][step[1]]="."
+		else:
+			board[step[0]][step[1]]= "◻"
 
 def boom(step,board):
 	if board[step[0]][step[1]]==".":
@@ -219,7 +174,7 @@ def boom(step,board):
 
 def main():
 	#print("Размер поля: ", end="")
-	y,x = 0, 0
+	y,x = 20, 40
 	#y,x = list(map(int, input().split(" ")))
 	#print("Количество бомб: ", end="")
 	bombs = 40
@@ -232,15 +187,14 @@ def main():
 	#step = list(map(int, input().split(" ")))
 	firstTurn(board,step,bombs)
 	print(printBoard(board))
-	return
 	while True:
 		print(printBoard(board))
 		print("Режим: ", end="")
-		mod = input()
+		mod = int(input())
 		print("Ход: ", end="")
-		step = input().split()
+		step = list(map(int, input().split()))
 		turn(board,step,mod)
-		if mod == 1:
+		if mod == 0:
 			if boom(step,board):
 				break
 	'''
